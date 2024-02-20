b4046d3b140bffccf1ae2662cf48f5c7f62ac948
Josue Benavides <jdbs9514@gmail.com>
2024-02-20 01:26:58 -0500

    set up with postgres and deploy de app

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,9 @@ const loadTask = async () => {
   return await prisma.task.findMany();
 }
 
+// agregar este metodo para que se actualice cada 60 segundos el estado del deploy
+export const revalidate = 60;
+
 const HomePage = async () => {
   const tasks = await loadTask();
   console.log(tasks);
